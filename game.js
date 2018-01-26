baa9583b71f6e2a60b63222a738ea5f9168c2ee4
Townsend2014 <saymay.townsend@gmail.com>
2018-01-25 18:25:38 -0700

    Updating comments

---
+++ game.js
@@ -0,0 +1,3 @@
+// Keep track of user's remaining guesses 
+
+// Prompt user to end game if there are no guesses left
---
+++ word.js
@@ -0,0 +1,15 @@
+// * **Word**: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
+
+// Import letter.js
+
+var letter = require('./letter.js');
+
+// Create Word constructor
+function Word () {
+
+
+
+}
+
+
+
